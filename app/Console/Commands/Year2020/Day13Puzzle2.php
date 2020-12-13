94f6a2ef54762a0f90d6592b06096554bb9a2421
Victor Dekker <victor@samson-it.nl>
2020-12-13 10:12:43 +0100

    Add day 13 puzzle 2 solution

---
+++ app/Console/Commands/Year2020/Day13Puzzle2.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands\Year2020;
+
+use Illuminate\Support\Collection;
+
+class Day13Puzzle2 extends Day13Puzzle1
+{
+    protected int $part = 2;
+
+    /**
+     * Solve the puzzle.
+     *
+     * @param string $data
+     *
+     * @return void
+     */
+    protected function solve(string $data)
+    {
+        list ($timestamp, $busses) = $this->parseData($data);
+
+        $busses = $busses->filter(function ($bus) {
+            return $bus != 'x';
+        });
+
+        $timestamp = 0;
+        $progress = $this->output->createProgressBar();
+
+        while (! $this->validateTimestamp($timestamp, $busses)) {
+            $timestamp++;
+            $progress->advance();
+        }
+
+        $progress->finish();
+
+        $this->answer = $timestamp;
+    }
+
+    protected function validateTimestamp($timestamp, Collection $busses)
+    {
+        foreach ($busses as $key => $bus) {
+            if (($timestamp + $key) % $bus != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
